a20463f9362c8dcb5146667a66ed6df830756d6d
xfin3st <98297783+xfin3st@users.noreply.github.com>
2025-09-04 02:20:43 +0200

    ready.js aktualisieren

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,3 +1,14 @@
 module.exports = (client) => {
   console.log(`✅ Eingeloggt als ${client.user.tag}`);
+
+  // Standard-Presence beim Start
+  client.user.setPresence({
+    activities: [
+      {
+        name: 'fin3st.de 👑', // dein Text hier
+        type: 3               // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching, 5 = Competing
+      }
+    ],
+    status: 'online' // online | idle | dnd | invisible
+  });
 };
